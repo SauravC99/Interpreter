fd2edbedeeaa03dfa7a3bcd8e1f897d553bf23d1
Saurav <sauravc41@gmail.com>
2025-02-15 00:08:08 -0800

    initial parser

---
+++ parser/parser.go
@@ -0,0 +1,53 @@
+package parser
+
+import (
+	"github.com/sauravc99/monkey-interpreter/ast"
+	"github.com/sauravc99/monkey-interpreter/lexer"
+	"github.com/sauravc99/monkey-interpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens to set curToken and peekToken
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return nil
+	default:
+		return nil
+	}
+}
