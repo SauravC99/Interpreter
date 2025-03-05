99fc666ac79d0113cc85909e525853afe5185c06
Saurav <sauravc41@gmail.com>
2025-03-04 23:47:26 -0800

    add support for evaluating boolean literals

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,13 @@ import (
 	"github.com/sauravc99/monkey-interpreter/object"
 )
 
+// Dont want to create new instances of these types each time since all are same
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
@@ -16,6 +23,9 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -29,4 +39,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
