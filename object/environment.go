3b29cdd1024c082aea75ec813df4197e009cfad3
Saurav <sauravc41@gmail.com>
2025-03-25 23:15:17 -0700

    refactor environment

--- object/environment.go
+++ object/environment.go
@@ -5,15 +5,15 @@ type Environment struct {
 	outer *Environment
 }
 
+func NewEnvironment() *Environment {
+	s := make(map[string]Object)
+	return &Environment{store: s, outer: nil}
+}
+
 func NewEnclosedEnvironment(outer *Environment) *Environment {
 	env := NewEnvironment()
 	env.outer = outer
 	return env
-}
-
-func NewEnvironment() *Environment {
-	s := make(map[string]Object)
-	return &Environment{store: s, outer: nil}
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
